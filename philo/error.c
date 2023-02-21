d0c0152e3d278b65172138d6fa2cd107413c58e5
Mohamed Aneddame <maneddam@e1r13p9.1337.ma>
2023-02-21 03:42:50 +0100

    philo

--- philo/error.c
+++ philo/error.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/21 01:34:24 by maneddam          #+#    #+#             */
-/*   Updated: 2023/02/21 03:09:40 by maneddam         ###   ########.fr       */
+/*   Created: 2023/02/21 03:09:03 by maneddam          #+#    #+#             */
+/*   Updated: 2023/02/21 03:32:35 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <pthread.h>
-# include <stdio.h>
-# include <string.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-typedef struct s_args
+void	print_error(char *msg)
 {
-	int philos_num;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int option_arg;
-}			t_args;
-
-void	print_error(char *msg);
-long int	ft_atoi(const char *str);
-
-
-#endif
+	printf("%s !!\n", msg);
+	exit(1);
+}
--- philo/philo.c
+++ philo/philo.c
@@ -6,51 +6,38 @@
 /*   By: maneddam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 01:38:38 by maneddam          #+#    #+#             */
-/*   Updated: 2023/02/21 01:40:06 by maneddam         ###   ########.fr       */
+/*   Updated: 2023/02/21 03:38:47 by maneddam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// C program to show thread functions
-
 #include "philo.h"
 
-void	*func(void *arg)
+void	get_infos(t_args *info, int argc, char *argv[])
 {
-	// detach the current thread
-	// from the calling thread
-	(void)arg;
-	pthread_detach(pthread_self());
-	printf("Inside the thread\n");
-	// exit the current thread
-	pthread_exit(NULL);
+	if (argc == 6)
+			info->option_arg = 1;
+	info->philos_num = ft_atoi(argv[1]);
+	info->time_to_die = ft_atoi(argv[2]);
+	info->time_to_eat = ft_atoi(argv[3]);
+	info->time_to_sleep = ft_atoi(argv[4]);
+	if (info->option_arg)
+		info->option_arg = ft_atoi(argv[5]);
 }
 
-void	fun(void)
+int	main(int argc, char *argv[])
 {
-	pthread_t	ptid;
+	t_args info;
+	if (argc == 5 || argc == 6)
+	{
+		get_infos(&info, argc, argv);
+		// numbering_philos();
 
-	// Creating a new thread
-	pthread_create(&ptid, NULL, &func, NULL);
-	printf("This line may be printed"
-			" before thread terminates\n");
-	// The following line terminates
-	// the thread manually
-	// pthread_cancel(ptid);
-	// Compare the two threads created
-	if (pthread_equal(ptid, pthread_self()))
-		printf("Threads are equal\n");
-	else
-		printf("Threads are not equal\n");
-	// Waiting for the created thread to terminate
-	pthread_join(ptid, NULL);
-	printf("This line will be printed"
-			" after thread ends\n");
-	pthread_exit(NULL);
-}
-
-// Driver code
-int	main(void)
-{
-	fun();
+		printf("number of philo and forks : %d\n", info.philos_num);
+		printf("time to die : %d milliseconds\n", info.time_to_die);
+		printf("time to eat : %d milliseconds\n", info.time_to_eat);
+		printf("time to sleep : %d milliseconds\n", info.time_to_sleep);
+		if (info.option_arg)
+			printf("number of times each philosopher must eat : %d times\n", info.option_arg);
+	}
 	return (0);
 }
